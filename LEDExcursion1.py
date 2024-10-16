4901de31e92e5885f845d4cf75b936f4c23dd666
Sasha <129900102+follosoraptor@users.noreply.github.com>
2024-10-15 22:04:39 -0400

    now includes threading :0
    reactionTime is not working how i want it to like why is it returning a negative number

--- LEDExcursion1.py
+++ LEDExcursion1.py
@@ -31,57 +31,134 @@ The results of this excursion will be used to define:
 # =============================================================================
 
 # Libraries
+import numpy as np
+import threading
 import time
 from pynput.keyboard import Key, Listener
 
 # Parameters to Set
 accuracyWindow = 2 #in seconds [just a placeholder for now]
+currentScore = 0
+keyPress = False
 
 # Function Definitions
-def testBeatMap():
-    testBeatTimes = [1]
+def testBeatMap(): #works fine
+    """Loads array of values (in seconds) in which hypothetical beats would occur."""
+    testBeatTimes = np.array([2, 4, 7, 10, 11])
     return testBeatTimes
 
-def resetScore():
+
+def startCountdown(seconds): #works fine
+    """Starts counting down to beginning of song session."""
+    while seconds:
+        print(seconds)
+        time.sleep(1) # waits 1 second
+        seconds -= 1
+    print("Start!")
+    
+
+def resetScore(): #works fine
     """Resets the current score to zero."""
-    currentScore = 0
-    print('Score has been reset!\n')
+    global currentScore
+    currentScore = 0   
+    print('Score has been reset!')
     
 
 def beatIndicator():
-    """Flashes the LED and awaits input from user"""
-    expectingInput = true
+    """Flashes the LED and awaits input from user."""
+    global expectingInput # i didnt do anything with this yet but i feel like we will need it somewhere down the line
+    expectingInput = True
     #PUT LED FLASH CODE HERE
     flashTime = time.time() #takes the current time
+    return flashTime
     
 
-def checkAccuracy(flashTime, inputTime):
+def checkAccuracy(timeDifference): # needs to be edited cuz i changed stuff around in main
     """Calculates and prints time between LED flash and key press."""
     global currentScore
-    reactionTime = inputTime - flashTime #obtains difference in time (in seconds)
+    reactionTime = timeDifference #obtains difference in time (in seconds)
     print(reactionTime)
     
     if reactionTime <= accuracyWindow:
         currentScore += 1
-        print("Hit! +1 added to score! \n")
-        print("Current Score: ", currentScore, "\n")
+        print("Hit! +1 added to score!")
+        print("Current Score: ", currentScore)
         
     elif reactionTime > accuracyWindow:
-        print("Miss!\n")
+        print("Miss!")
         
-def userInput(key): #detects the time in which the spacebar was pressed
-    if key == Key.space:
-        print("Detected user input!\n")
-        inputTime = time.time()
-        print(inputTime, "\n")
 
-        
+def userInput(key): #works fine
+    """Detects the time in which the user hits the Spacebar."""
+    if key == Key.space:
+        global keyPress
+        global inputTime
+        keyPress = True
+        print("Detected user input!")    
+        inputTime = time.time()
+        print(inputTime)
+
+
+def stopUserInput(key): #works fine
+    """Stops listening for user inputs."""
+    if key == Key.esc:
+        return False
+    
+    
+def listenForInput(): #works fine
+    """Enables input monitoring."""
+    with Listener(on_press=userInput, on_release=stopUserInput) as listener:
+        listener.join()    
+     
   
 # Main Program
 if __name__ == '__main__':
-    resetScore()
-      
-    # Set up the listener for keyboard events
-    with Listener(on_press=userInput, on_release=None) as listener:
-        listener.join()
+    
+    # Initialization of new song session
+    resetScore() # sets current score equal to zero
+    beatMap = testBeatMap() # for demo purposes
+    startCountdown(5) # counts down to beginning of session
+    songInSession = True
+    
+    while songInSession == True:      
+        lastBeatTime = 0
+        beatWaitTimes = np.array([]) #creates empty array that will be appended
+        
+        #creates thread to listen for inputs concurrently as program continues
+        inputThread = threading.Thread(target = listenForInput) 
+        inputThread.start()
+        
+        #calculates timing for LED flashes
+        for currentBeatTime in beatMap:
+            print('Last Beat:', lastBeatTime)
+            timeDifference = currentBeatTime - lastBeatTime
+            lastBeatTime = currentBeatTime
+            print('Current Beat:', currentBeatTime)
+            print('Time Difference:', timeDifference)
+            beatWaitTimes = np.append(beatWaitTimes, timeDifference)
+            print('Current beatWaitTimes Array:', beatWaitTimes)
+        
+        #flashes LEDs and calculates accuracy
+        for x in beatWaitTimes: #this part is still scuffed
+            time.sleep(x)
+            print("LED BLINK RAH")
+            flashTime = time.time()
+            if keyPress == True: # executes only if spacebar was pressed
+                print("LED flash time: ", flashTime)
+                print("input time: ", inputTime)
+                reactionTime = inputTime - flashTime
+                print("Reaction Time: ", reactionTime)  
+                keyPress = False
+            
+     
+            
+        songInSession = False
+        
+    print("This is your final score!", currentScore)
+        
+            
+            
+    
+        
+            
     
