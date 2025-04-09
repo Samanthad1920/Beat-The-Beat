48f1ce92a9a9ca05e4276b078c78813a06af8c36
Sasha <129900102+follosoraptor@users.noreply.github.com>
2025-04-09 11:35:45 -0400

    Create Speaker_test.py

---
+++ Verification and Validation/Speaker_test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Beat the Beat! System Test Verification
+Speaker Component
+"""
+
+# Libraries
+from pygame import mixer
+
+SONG_FILE = "test.mp3"
+
+# Functions
+def playSong():
+    """Plays loaded song file."""
+    mixer.music.play()
+    
+def stopSong():
+    """Stops the currently playing song."""
+    mixer.music.stop()
+
+# Main Program
+if __name__ == '__main__':
+    mixer.init()
+    mixer.music.load(SONG_FILE)
+    playSong()
+    input("Press enter to stop music playback!")
+    stopSong() 
